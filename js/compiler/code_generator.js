7a090b6712ec012afe9b8eb94150776e22a8428d
n <avdosev@gmail.com>
2018-12-08 19:05:20 +0300

    генератор готов, дальше косметические работы. займусь курсачем

--- js/compiler/code_generator.js
+++ js/compiler/code_generator.js
@@ -1,9 +1,29 @@
 export function codeGenerator(derevo, degenerationTable, arrayOfIdent, arrayOfConst) {
     //ну шо генератор
     function generatorStr(obj) {//fix
-        var newstr = '';
-        
-        return newstr;
+        var str = '';
+        var ptab = degenerationTable[obj["type"]];
+        if (ptab) {
+            if (obj.value) {
+                if (obj.type == "<value_id>") {
+                    str += `${arrayOfIdent[obj.value[1]]}`;
+                } else if (obj["type"] == "<value_num>") {
+                    str += `${arrayOfConst[obj.value[1]]}`;
+                } else {
+                    console.log("не понятное значение");
+                    throw 0;
+                }
+            }
+            else {
+               str += ptab["before"];
+               str += walk(obj.body);
+               str += ptab["after"];
+            }
+        }
+        else {
+            str += walk(obj.body);
+        }
+        return str;
     }
 
     //гуляем по древу
